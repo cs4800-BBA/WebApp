8e1f6c9c5631ead23bb9329de1f0917e04c4e152
Benjamin Dinh <benjadinh@gmail.com>
2023-10-20 15:36:14 -0700

    Test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState('');
+
+  const handleAddTask = () => {
+    if (task.trim() !== '') {
+      setTasks([...tasks, task]);
+      setTask('');
+    }
+  };
+
+  const handleDeleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div className="App">
+      <h1>React To-Do List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Add a task..."
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button onClick={handleAddTask}>Add</button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => handleDeleteTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
