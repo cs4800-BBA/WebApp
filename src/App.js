daedeee9587d641dc7a076357114ca83d0c417dd
Benjamin Dinh <benjadinh@gmail.com>
2023-10-22 12:10:50 -0700

    Test 3

--- src/App.js
+++ src/App.js
@@ -1,45 +1,13 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import TaskList from './components/TaskList';
-import AddTask from './components/AddTask';
+import React from "react";
+import ReactDOM from "react-dom";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-
-  const addTask = (task) => {
-    setTasks([...tasks, task]);
-  };
-
-  const deleteTask = (index) => {
-    const updatedTasks = tasks.filter((_, i) => i !== index);
-    setTasks(updatedTasks);
-  };
-
   return (
-    <Router>
-      <div className="App">
-        <h1>Task Management App</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/add-task">Add Task</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route exact path="/">
-            <TaskList tasks={tasks} deleteTask={deleteTask} />
-          </Route>
-          <Route path="/add-task">
-            <AddTask addTask={addTask} />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div>
+      <h1>My React Website</h1>
+      <p>This is a simple React website.</p>
+    </div>
   );
 }
 
-export default App;
+ReactDOM.render(<App />, document.getElementById("root"));
